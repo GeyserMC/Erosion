07cec2f4bd037844af5409e7acdd8115bc78f82b
Camotoy <20743703+Camotoy@users.noreply.github.com>
2023-04-06 13:39:59 -0400

    Test and see if this still publishes and makes the errors go away

--- build-logic/src/main/kotlin/erosion.publish-conventions.gradle.kts
+++ build-logic/src/main/kotlin/erosion.publish-conventions.gradle.kts
@@ -5,16 +5,4 @@ plugins {
 indra {
     publishSnapshotsTo("geysermc", "https://repo.opencollab.dev/maven-snapshots")
     publishReleasesTo("geysermc", "https://repo.opencollab.dev/maven-releases")
-
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                groupId = project.group as String
-                artifactId = project.name
-                version = project.version as String
-
-                from(components["java"])
-            }
-        }
-    }
 }
